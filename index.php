19ea5e44b7059b652ee5538eb390391f6f7dfbe0
Akansha rana <44633873+akansharana@users.noreply.github.com>
2019-10-31 11:45:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+in this code we will use two methods
 <?php
 $page = $_SERVER['PHP_SELF'];
 $sec = "10";
@@ -12,3 +13,21 @@ $sec = "10";
     ?>
     </body>
 </html>
+     or
+
+<?php 
+  
+// Demonstrate the use of header() function 
+// to refresh the current page 
+   
+echo "Welcome to index page</br>"; 
+echo "Page will refresh in every 3 seconds</br></br>"; 
+    
+// The function will refresh the page  
+// in every 3 second 
+header("refresh: 3"); 
+    
+echo date('H:i:s Y-m-d'); 
+  
+exit; 
+?>
